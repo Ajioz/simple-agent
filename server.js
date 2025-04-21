707b6b375a8c86e367e92ded264fad3eab46329b
Ajioz <sunny.ajiroghene@gmail.com>
2025-04-21 17:55:51 +0100

    updated terms

--- server.js
+++ server.js
@@ -105,11 +105,3 @@ const port = process.env.PORT || 5000;
 const server = app.listen(port, () =>
   console.log(`App listening on port http://127.0.0.1:${port}...`)
 );
-// --- Graceful Shutdown ---
-process.on("SIGTERM", () => {
-  console.info("SIGTERM received. Closing server...");
-  server.close(() => {
-    console.info("Server closed.");
-    process.exit(0);
-  });
-});
